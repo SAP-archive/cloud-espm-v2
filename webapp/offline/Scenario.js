ca6004b24fe8b5e4c4b20bc53c44b1f5c7c452c3
navin <navin.krishnan.manohar@sap.com>
2017-02-28 15:43:46 +0530

    Cleaned up folders

--- webapp/offline/Scenario.js
+++
@@ -1,8 +0,0 @@
-jQuery.sap.declare("com.sap.espm.shop.offline.Scenario");
-com.sap.espm.shop.offline.Scenario = {
-// The init function can be used to manipulate the local launchpad or for an time 
-// based sync mechanismus (sync every 5 Minutes).
-    init: function () {
-
-	}
-};
