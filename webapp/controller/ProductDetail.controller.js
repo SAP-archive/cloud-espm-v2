1160824efe68bd8ac256daa660e1ad0ef9522a7e
Manohar Navin Krishnan <navin.krishnan.manohar@sap.com>
2017-03-28 10:49:56 +0000

    Changed Odata read to AJAX for Fiori mobile compatible

--- webapp/controller/ProductDetail.controller.js
+++ webapp/controller/ProductDetail.controller.js
@@ -7,29 +7,29 @@ sap.ui.define([
 	"use strict";
 	var bindingObject, bindingPath;
 	return Controller.extend("com.sap.espm.shop.controller.ProductDetail", {
-		
+
 		formatter: formatter,
 		sortReviewDesc: true,
-		onInit:function()
-		{
+		onInit: function() {
 			var that = this;
 			var oComponent = this.getOwnerComponent();
 			var oModel = oComponent.getModel("Cart");
-		  	this.getView().addEventDelegate({
+			this.getView().addEventDelegate({
 				onAfterShow: function() {
-					 that.getView().byId("btnProductHeader").setText(formatter.onAddCountToCart(oModel));
-				}});
-			
+					that.getView().byId("btnProductHeader").setText(formatter.onAddCountToCart(oModel));
+				}
+			});
+
 			var oRouter = UIComponent.getRouterFor(this);
 			oRouter.getRoute("ProductDetail").attachPatternMatched(this._onObjectMatched, this);
-			
+
 			this._oReviewDialog = null;
 
 		},
-		_onObjectMatched: function (oEvent) {
+		_onObjectMatched: function(oEvent) {
 			bindingObject = oEvent.getParameter("arguments").Productdetails;
-			bindingPath = "/"+bindingObject;
-			bindingObject = "EspmModel>/"+bindingObject;
+			bindingPath = "/" + bindingObject;
+			bindingObject = "EspmModel>/" + bindingObject;
 			this.getView().bindElement(bindingObject);
 			this.byId("reviewTable").bindItems({
 				path: bindingObject + "/CustomerReview",
@@ -39,20 +39,18 @@ sap.ui.define([
 			var oTable = oView.byId("reviewTable");
 
 			var oBinding = oTable.getBinding("items");
-		//Sorter part commented	
-		//	var sorters = new sap.ui.model.Sorter("CreationDate", true);
-		//	oBinding.sort(sorters);
+			//Sorter part commented	
+			//	var sorters = new sap.ui.model.Sorter("CreationDate", true);
+			//	oBinding.sort(sorters);
 		},
 
-		onAfterRendering: function() 
-		{
-			
+		onAfterRendering: function() {
+
 		},
-		onBeforeRendering: function() 
-		{
-			
+		onBeforeRendering: function() {
+
 		},
-		onNavBack: function () {
+		onNavBack: function() {
 			var oHistory = History.getInstance();
 			var sPreviousHash = oHistory.getPreviousHash();
 
@@ -63,42 +61,41 @@ sap.ui.define([
 				oRouter.navTo("Home", true);
 			}
 		},
-		onSupplierPressed : function(oEvent){
+		onSupplierPressed: function(oEvent) {
 			var oLink = oEvent.getSource();
 			if (!this._oSupplierCard) {
 				this._initializeSupplierCard();
-			}
-			else{
+			} else {
 				this._oSupplierCard.bindElement({
-				path: bindingObject + "/Supplier"
-				
-			});
-			
+					path: bindingObject + "/Supplier"
+
+				});
+
 			}
 			this._oSupplierCard.openBy(oLink);
-			
+
 		},
 		_initializeSupplierCard: function() {
-			this._oSupplierCard = sap.ui.xmlfragment("com.sap.espm.shop.view.fragment.SupplierCard",this.getView());
+			this._oSupplierCard = sap.ui.xmlfragment("com.sap.espm.shop.view.fragment.SupplierCard", this.getView());
 			this._oSupplierCard.bindElement({
 				path: bindingObject + "/Supplier"
 			});
 			this.getView().addDependent(this._oSupplierCard);
 		},
-		onAddToCartPressed: function(){
-			
+		onAddToCartPressed: function() {
+
 			var oModel = this.getView().getModel("Cart");
 			var model = this.getView().getModel("EspmModel");
-	        var productContext = model.getProperty(bindingPath);
-	        formatter.onAddToCart(oModel,productContext);
-	        this.getView().byId("btnProductHeader").setText(formatter.onAddCountToCart(oModel));
-			
+			var productContext = model.getProperty(bindingPath);
+			formatter.onAddToCart(oModel, productContext);
+			this.getView().byId("btnProductHeader").setText(formatter.onAddCountToCart(oModel));
+
 		},
-		onShoppingCartPressed: function(){
-			
+		onShoppingCartPressed: function() {
+
 			var oRouter = UIComponent.getRouterFor(this);
 			oRouter.navTo("Shoppingcart");
-			
+
 		},
 		_createDialog: function(sDialog) {
 			var oDialog = sap.ui.xmlfragment(sDialog, this);
@@ -107,7 +104,7 @@ sap.ui.define([
 			return oDialog;
 		},
 		onEditReviewPressed: function() {
-			
+
 			if (!this._oReviewDialog) {
 				this._oReviewDialog = this._createDialog("com.sap.espm.shop.view.fragment.ReviewDialog");
 			}
@@ -116,47 +113,54 @@ sap.ui.define([
 		onReviewDialogOKPressed: function(oEvent) {
 			var that = this;
 			var oBundle = this.getView().getModel('i18n').getResourceBundle();
-			
+
 			this._oReviewDialog.close();
 			var iRatingCount = sap.ui.getCore().byId("ratingIndicator", "reviewDialog").getValue();
 			var sReviewComment = sap.ui.getCore().byId("textArea", "reviewDialog").getValue();
 			var firstName = sap.ui.getCore().byId("firstNameId", "reviewDialog").getValue();
 			var lastName = sap.ui.getCore().byId("lastNameId", "reviewDialog").getValue();
-			var createtionDate = new Date().getTime(); 
+			var createtionDate = new Date().getTime();
 			var prodId = this.byId("productId").getText();
-			
-			var sFunctionImportReviewParam = "ProductId='" + prodId + "'&FirstName='" + firstName + "'&LastName='" + lastName + "'&Rating='" + iRatingCount + "'&CreationDate='" + createtionDate + "'&Comment='" + sReviewComment + "'";
-		
+
+			var sFunctionImportReviewParam = "ProductId='" + prodId + "'&FirstName='" + firstName + "'&LastName='" + lastName + "'&Rating='" +
+				iRatingCount + "'&CreationDate='" + createtionDate + "'&Comment='" + sReviewComment + "'";
+
 			var aParams = [];
 			aParams.push(sFunctionImportReviewParam);
-			
+
 			var oModel = this.getView().getModel("EspmModel");
-			
-			oModel.setHeaders({  
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        	});
-			oModel.read('/CreateCustomerReview',null, aParams , false, function(data)
-			{
- 				if(data){
- 					sap.m.MessageToast.show(oBundle.getText("detail.reviewSuccess"));
-					// Clear review dialog
-					that.clearReviewDialogForm();
- 					
- 				}
- 			},function(){
-				sap.m.MessageToast.show(oBundle.getText("detail.reviewFailed")); 
- 			});
-			oModel.refresh(true);
-			
+
+			$.ajax({
+				type: "GET",
+				async: true,
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				url: "/ESPM/espm-cloud-web/espm.svc/CreateCustomerReview?" + sFunctionImportReviewParam,
+				success: function(data) {
+
+					if (data) {
+						sap.m.MessageToast.show(oBundle.getText("detail.reviewSuccess"));
+						// Clear review dialog
+						that.clearReviewDialogForm();
+						oModel.refresh(true);
+
+					}
+
+				},
+				error: function() {
+					sap.m.MessageToast.show(oBundle.getText("detail.reviewFailed"));
+				}
+			});
+
+
 			var oView = this.getView();
 			var oTable = oView.byId("reviewTable");
 
 			var oBinding = oTable.getBinding("items");
-			
+
 			var sorters = new sap.ui.model.Sorter("CreationDate", true);
 			oBinding.sort(sorters);
-			
+
 		},
 
 		// Close the Review Dialog
@@ -165,7 +169,7 @@ sap.ui.define([
 			this.clearReviewDialogForm();
 		},
 
-		clearReviewDialogForm: function(){
+		clearReviewDialogForm: function() {
 			sap.ui.getCore().byId("ratingIndicator", "reviewDialog").setValue(0);
 			sap.ui.getCore().byId("textArea", "reviewDialog").setValue("");
 			sap.ui.getCore().byId("firstNameId", "reviewDialog").setValue("");
@@ -180,20 +184,19 @@ sap.ui.define([
 			}
 		},
 
-		validateReviewForm: function(){
+		validateReviewForm: function() {
 			var iRatingCount = sap.ui.getCore().byId("ratingIndicator", "reviewDialog").getValue();
 			var sReviewComment = sap.ui.getCore().byId("textArea", "reviewDialog").getValue();
 			var firstName = sap.ui.getCore().byId("firstNameId", "reviewDialog").getValue();
 			var lastName = sap.ui.getCore().byId("lastNameId", "reviewDialog").getValue();
 
-			if(iRatingCount > 0 && sReviewComment !== "" && firstName !== "" && lastName !== ""){
+			if (iRatingCount > 0 && sReviewComment !== "" && firstName !== "" && lastName !== "") {
 				sap.ui.getCore().byId("btnOK", "reviewDialog").setEnabled(true);
-			}
-			else{
+			} else {
 				sap.ui.getCore().byId("btnOK", "reviewDialog").setEnabled(false);
 			}
 		},
-		onTableSettingsPressed: function(){
+		onTableSettingsPressed: function() {
 			var oBinding = this.byId("reviewTable").getBinding("items");
 			var aSorters = [];
 			var aDescending = this.sortReviewDesc;
@@ -203,7 +206,6 @@ sap.ui.define([
 			oBinding.sort(aSorters);
 
 		}
-		
 
 	});
 
